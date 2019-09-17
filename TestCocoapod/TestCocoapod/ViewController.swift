ac26d494f6a9b60181f1ffcfc08044a003943090
Paul Solt <PaulSolt@gmail.com>
2019-09-17 16:36:42 -0400

    Added starter project

---
+++ TestCocoapod/TestCocoapod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCocoapod
+//
+//  Created by Paul Solt on 9/17/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
